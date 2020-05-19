b32dea2c76fbb1f7067ea2021928abb6d806df1d
nemus <nemanjadjekic93@gmail.com>
2020-05-20 01:32:09 +0200

    Solution

--- Day 1 -JavaScript Drum Kit/index.js
+++ Day 1 -JavaScript Drum Kit/index.js
@@ -1,11 +1,11 @@
-var numOfDrumKeys = document.querySelectorAll("kbd").length;
+var numOfDrumKeys = document.querySelectorAll(".key").length;
 
 // Detecting Button Press
 for (var i = 0; i < numOfDrumKeys; i++) {
-  document.querySelectorAll("kbd")[i].addEventListener("click", function () {
-    var btnInnerHTML = this.innerHTML;
-    console.log("Clicked on ", btnInnerHTML);
-    makeSound(btnInnerHTML);
+  document.querySelectorAll(".key")[i].addEventListener("click", function () {
+    var keyInner = this.innerText[0];
+    makeSound(keyInner);
+    keyAnimation(keyInner.toLowerCase());
   });
 }
 
@@ -20,57 +20,75 @@ function makeSound(key) {
     case "a":
       var clap = new Audio("sounds/clap.wav");
       clap.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "S":
     case "s":
       var hihat = new Audio("sounds/hihat.wav");
       hihat.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "D":
     case "d":
       var kick = new Audio("sounds/kick.wav");
-      kick.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "F":
     case "f":
       var openhat = new Audio("sounds/openhat.wav");
       openhat.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "G":
     case "g":
       var boom = new Audio("sounds/boom.wav");
       boom.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "H":
     case "h":
       var ride = new Audio("sounds/ride.wav");
       ride.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "J":
     case "j":
       var snare = new Audio("sounds/snare.wav");
       snare.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "K":
     case "k":
       var tom = new Audio("sounds/tom.wav");
       tom.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     case "L":
     case "l":
       var tink = new Audio("sounds/tink.wav");
       tink.play();
+      keyAnimation(key.toLowerCase());
       break;
 
     default:
       console.log();
   }
+}
+
+function keyAnimation(currentKey) {
+  var activeKey = document.querySelector("." + currentKey);
+
+  activeKey.classList.add("playing");
+
+  setTimeout(function () {
+    activeKey.classList.remove("playing");
+  }, 200);
 }
