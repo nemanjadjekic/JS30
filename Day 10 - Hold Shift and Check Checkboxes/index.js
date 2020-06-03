f88085d1c094c7e45a7531467fb5476a0f31ee8f
Nemanja Djekic <nemanja.djekic@continental-corporation.com>
2020-06-03 16:15:25 +0200

    Day 10 Finished

--- Day 10 - Hold Shift and Check Checkboxes/index.js
+++ Day 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,1 +1,23 @@
-console.log("Hello!");
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+	// Check if they had the shift key down
+	let inBetween = false;
+	if(e.shiftKey && this.checked){
+		checkboxes.forEach(checkbox => {
+			console.log(checkbox);
+			if(checkbox === this || checkbox === lastChecked){
+				inBetween = !inBetween;
+			} 
+			
+			if(inBetween){
+				checkbox.checked = true;
+			}
+		});
+	}
+	lastChecked = this;
+};
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
