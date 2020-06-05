45b9aee088fead6056cc6e2db61d7c8cd37e0f68
nemanjadjekic <nemanjadjekic93@gmail.com>
2020-06-05 19:10:49 +0200

    Day 14 Done

--- Day 14 - Reference vs Copy/index.js
+++ Day 14 - Reference vs Copy/index.js
@@ -1,9 +1,25 @@
 // start with strings, numbers and booleans
-console.log("Hello World!");
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
+
+// let name = "Test";
+// let name2 = name;
+// console.log(name, name2);
+// name = "new ";
+// console.log(name, name2);
+
 // Let's say we have an array
 const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 
 // and we want to make a copy of it.
+const team = players;
+
+// console.log(players, team);
+// team[3] = "Lux";
+// console.log(players, team);
 
 // You might think we can just do something like this:
 
@@ -16,12 +32,22 @@ const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
+// const team2 = players.slice();
+// console.log(players, team2);
 
 // one way
 
 // or create a new array and concat the old one in
+// const team3 = [].concat(players);
+// console.log(players, team3);
 
 // or use the new ES6 Spread
+// const team4 = [...players];
+// team4[3] = "Heey!";
+// console.log(players, team4);
+
+// const team5 = Array.from(players);
+// console.log(players, team5);
 
 // now when we update it, the original one isn't changed
 
@@ -34,9 +60,33 @@ const person = {
 };
 
 // and think we make a copy:
+// const captain = person;
+// captain.number = 99;
+// console.log(person, captain);
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99, age: 12 });
+console.log(person, cap2);
 
 // We will hopefully soon see the object ...spread
+const cap3 = { ...person };
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+const test = {
+  name: "Test",
+  age: 100,
+  social: {
+    twitter: "@test",
+    facebook: "@test.dev",
+  },
+};
+
+console.clear();
+console.log(test);
+
+const dev = Object.assign({}, test);
+console.log(dev);
+
+const dev2 = JSON.parse(JSON.stringify(test));
+console.log(dev2);
