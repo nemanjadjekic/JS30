0e03e3236b4ec21071852ccf03600409dc384263
nemanjadjekic <nemanjadjekic93@gmail.com>
2020-06-10 21:51:46 +0200

    Day 17 Done

--- Day 17 - Sorting Band Names/index.js
+++ Day 17 - Sorting Band Names/index.js
@@ -14,4 +14,12 @@ const bands = [
   "An Old Dog",
 ];
 
-console.log(bands);
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join("");
