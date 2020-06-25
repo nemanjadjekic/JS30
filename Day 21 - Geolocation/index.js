6b8ad7a6c41ec608936a5d6227145d67734d95c3
nemanjadjekic <nemanjadjekic93@gmail.com>
2020-06-25 15:24:44 +0200

    Day 21 Done

--- Day 21 - Geolocation/index.js
+++ Day 21 - Geolocation/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+  }
+);
